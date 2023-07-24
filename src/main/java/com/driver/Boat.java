71696f02cc2ede00e98835f51111ce9a7616d1f3
kuspilla <118829965+kuspilla@users.noreply.github.com>
2023-07-24 11:00:20 +0530

    DONE

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -8,31 +8,15 @@ public  class Boat implements WaterVehicle{
         this.name= name;
         this.Capacity = Capacity;
     }
-        public  Boat(String naame){
-        this.name =name;
-        }
-        public Boat(int Capacity){
-        this.Capacity = Capacity;
-        }
+    public Boat(){}
 
 
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
-    public void setCapacity(int capacity) {
-        this.Capacity = capacity;
-    }
-
-@Override
     public String getVehicleName() {
         return this.name;
     }
 
-@Override
+
     public int getVehicleCapacity() {
         return this.Capacity;
     }
